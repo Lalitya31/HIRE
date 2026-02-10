7b9e47959c50f8b6a695739591ff40930968772b
Lalitya <lalityadodla@gmail.com>
2026-02-10 11:36:27 +0530

    fix routing to preserve feed at root path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ import OnboardingPage from './pages/OnboardingPage';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<LandingPage />} />
+      <Route path="/" element={<FeedPage />} />
       <Route path="/feed" element={<FeedPage />} />
       <Route path="/onboarding" element={<OnboardingPage />} />
       <Route path="/applications/:id" element={<ApplicationPreviewPage />} />

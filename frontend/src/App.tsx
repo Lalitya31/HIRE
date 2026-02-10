9af2985ed8204461d267b8b282f0f94fd12c9667
Lalitya <lalityadodla@gmail.com>
2026-02-10 10:54:07 +0530

    Implement landing page with basic styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
 import FeedPage from './pages/FeedPage';
 import ApplicationPreviewPage from './pages/ApplicationPreviewPage';
 import OnboardingPage from './pages/OnboardingPage';
@@ -6,7 +7,8 @@ import OnboardingPage from './pages/OnboardingPage';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<FeedPage />} />
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/feed" element={<FeedPage />} />
       <Route path="/onboarding" element={<OnboardingPage />} />
       <Route path="/applications/:id" element={<ApplicationPreviewPage />} />
     </Routes>
